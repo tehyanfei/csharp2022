4400d6e55b3f1a8e80205af2f909601d9b14edea
tehyanfei <yanfeiii04@163.com>
2022-10-18 19:56:30 +0800

    1st commit

---
+++ MyFirstCSharp/MyFirstCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharpOne
+{
+
+    /// <summary>
+    /// 输出直角三角形
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stars = "*";
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine(stars);
+                stars += "*";
+            }
+        }
+    }
+
+}
---
+++ MySecondCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharpTwo
+{
+    /// <summary>
+    /// Moving game
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //设函数x,y
+            int x, y;
+
+            //把string输出在x,y的位置
+            Console.Clear();
+            Console.SetCursorPosition(x, y);
+            Console.Write("Hello");
+
+            //使用Console.Readkey输入
+        }
+    }
+}
+
